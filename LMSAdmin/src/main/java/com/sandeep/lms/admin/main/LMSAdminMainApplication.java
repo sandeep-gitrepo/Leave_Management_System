4d24a9a9ab545523cf1b7459392a30fde5f34c00
Sandeep Kumar <sandeep.mehta.mca@gmail.com>
2020-10-20 09:58:36 +0530

    Fixed issue

--- LMSAdmin/src/main/java/com/sandeep/lms/admin/main/LMSAdminMainApplication.java
+++ LMSAdmin/src/main/java/com/sandeep/lms/admin/main/LMSAdminMainApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author sandeep.a.kumar
  *
  */
-@SpringBootApplication(scanBasePackages = {"com.sandeep.lms.admin"})
+@SpringBootApplication(scanBasePackages = {"com.sandeep.lms"})
 public class LMSAdminMainApplication {
 
 	public static void main(String[] args) {
